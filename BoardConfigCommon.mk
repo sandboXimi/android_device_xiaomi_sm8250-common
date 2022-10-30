9580b7313e4f4c820d296d4f1874c15c5c1b6866
Tengfei Fan <tengfeif@codeaurora.org>
2022-10-30 12:39:07 +0000

    sm8250-common: Make ramdisk compressed by lz4
    There is big benefit if use lz4 compress ramdisk for
    kernel boot KPI.
    
    Change-Id: Ifa97b18fd199621896ae30a96741a946613d5387
    Signed-off-by: Tengfei Fan <tengfeif@codeaurora.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,6 +101,7 @@ TARGET_KERNEL_CONFIG := \
     vendor/kona-perf_defconfig \
     vendor/debugfs.config \
     vendor/xiaomi/sm8250-common.config
+BOARD_RAMDISK_USE_LZ4 := true
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
