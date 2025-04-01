9d130cf46de51e6a14593c2ebdd29a42b61d6d92
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-04-01 18:10:52 +0200

    sm8250-common: Use hardware/xiaomi FCM
    Change-Id: Ib1dcdfe20ef5ffaf1b3b407b47790ee029a2755f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -224,8 +224,8 @@ endif
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 ifneq ($(TARGET_IS_TABLET),true)
